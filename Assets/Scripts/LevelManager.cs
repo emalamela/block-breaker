b70470ea1241028ef696dd5e001b0d03e0e07bbe
emalamela <lamelaemanuel@gmail.com>
2016-12-03 21:05:55 -0300

    Added font & music assets, applied fonts to Text Prefabs & music to MusicPlayer Prefab, got basic game flow going

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -4,33 +4,44 @@ using System.Collections;
 
 public class LevelManager : MonoBehaviour {
 
+	private const string LEVEL_SCENE_FORMAT_NAME = "Level_{0:D2}";
+	private const string LOSE_SCENE_NAME = "Lose";
+
+	private const string GAME_SCENE_NAME = "Game";
+	private const string WIN_SCENE_NAME = "Win";
+	private const string START_SCENE_NAME = "Start";
+
 	public void loadGameScene() {
-		loadLevel("Game");
+		loadScene(GAME_SCENE_NAME);
 	}
 
 	public void loadWinScene() {
-		loadLevel("Win");
+		loadScene(WIN_SCENE_NAME);
 	}
 
 	public void loadLoseScene() {
-		loadLevel("Lose");
+		loadScene(LOSE_SCENE_NAME);
 	}
 
 	public void loadStartScene() {
-		loadLevel("Start");
+		loadScene(START_SCENE_NAME);
 	}
 
-	private void loadLevel(string levelName) {
+	private void loadScene(string sceneName) {
 		var activeSceneName = SceneManager.GetActiveScene().name;
-		if (activeSceneName.ToLower().Equals(levelName.ToLower())) {
+		if (activeSceneName.ToLower().Equals(sceneName.ToLower())) {
 			print("Scene with name " + activeSceneName + " is already loaded.");
 			return;
 		}
 
-		SceneManager.LoadScene(levelName);
+		SceneManager.LoadScene(sceneName);
 	}
 
 	public void quitGame() {
 		Application.Quit();
+	}
+
+	public void loadLevel(int level) {
+		loadScene(string.Format(LEVEL_SCENE_FORMAT_NAME, level));
 	}
 }
---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+
+	void Start () {
+		GameObject.DontDestroyOnLoad(gameObject);
+	}
+	
+}
