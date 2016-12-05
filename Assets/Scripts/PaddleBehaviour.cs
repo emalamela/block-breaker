19d3747ec23765727f2c6c5d7b9cc51c1a4852a7
emalamela <lamelaemanuel@gmail.com>
2016-12-04 21:08:27 -0300

    Ball is attached to Paddle as long as it isn't launched, Paddle now follows mouse X position with caps

---
+++ Assets/Scripts/PaddleBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PaddleBehaviour : MonoBehaviour {
+
+	private const int GAME_WIDTH_IN_UNITS = 16;
+
+	public GameObject ball;
+	public Vector2 ballLaunchVelocity;
+
+	private bool hasLaunchedBall = false;
+
+	void Update() {
+		updatePaddlePosition();
+
+		if (!hasLaunchedBall) {
+			ball.transform.position = new Vector3(transform.position.x, ball.transform.position.y, transform.position.z);
+
+			if (Input.GetMouseButtonDown(0)) {
+				hasLaunchedBall = true;
+				ball.GetComponent<Rigidbody2D>().velocity = ballLaunchVelocity;
+			}
+		}
+	}
+
+	// Clamps the X position of the paddle to avoid it leaving the screen (this assumes paddle has 1 world unit of width)
+	private void updatePaddlePosition() {
+		transform.position = new Vector3(getMousePosInBlocks(), transform.position.y, 0f);
+	}
+
+	private static float getMousePosInBlocks() {
+		return Input.mousePosition.x / Screen.width * GAME_WIDTH_IN_UNITS;
+	}
+}
