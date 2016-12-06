88cc1bf3821e875834aed666af1c958b0a2c6d77
emalamela <lamelaemanuel@gmail.com>
2016-12-06 20:25:09 -0300

    Re-organized prefabs in sub-folders, added 3 types of bricks, set-up play space

---
+++ Assets/Scripts/BrickBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BrickBehaviour : MonoBehaviour {
+
+	public int hitThreshold;
+
+	[SerializeField]
+	private int hitTimes;
+
+	void Start() {
+		hitTimes = 0;	
+	}
+
+	void OnCollisionEnter2D(Collision2D collision) {
+		hitTimes++;
+	}
+
+}
