aa989c1326ddcb87cbb41d9a9cb5e282a7c309a9
emalamela <lamelaemanuel@gmail.com>
2016-12-24 16:02:21 -0300

    Auto play support

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -6,16 +6,40 @@ public class Paddle : MonoBehaviour {
 	private const int GAME_WIDTH_IN_UNITS = 16;
 	private const string BALL_TAG = "Ball";
 
-	void Update() {
-		updatePaddlePosition();
+	public bool autoPlay;
+
+	private Ball ball;
+
+	void Start() {
+		ball = GameObject.FindObjectOfType<Ball>();
 	}
 
-	// Clamps the X position of the paddle to avoid it leaving the screen (this assumes paddle has 1 world unit of width)
-	private void updatePaddlePosition() {
-		transform.position = new Vector3(getMousePosInBlocks(), transform.position.y, 0f);
+	void Update() {
+		if (autoPlay) {
+			updatePositionWithBall();
+		} else {
+			updatePositionWithMouse();
+		}
+	}
+
+	private void updatePositionWithMouse() {
+		clampUpdatePosition(getMousePosInBlocks());
 	}
 
 	private static float getMousePosInBlocks() {
 		return Input.mousePosition.x / Screen.width * GAME_WIDTH_IN_UNITS;
+	}
+
+	private void updatePositionWithBall() {
+		clampUpdatePosition(ball.transform.position.x);
+	}
+
+	// Clamps the X position of the paddle to avoid it leaving the screen (this assumes paddle has 1 world unit of width)
+	private void clampUpdatePosition(float x) {
+		transform.position = 
+				new Vector3(
+						Mathf.Clamp(x, 0.5f, 15.5f), 
+						transform.position.y, 
+						0f);
 	}
 }
